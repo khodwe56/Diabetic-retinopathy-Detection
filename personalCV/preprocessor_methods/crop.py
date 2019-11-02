3addf9d5e02adca956763bc00b4a5314a155b760
Omkar Khodwe <bim2017007@iiita.ac.in>
2019-11-02 15:06:38 +0530

    Crop preprocessor added

--- personalCV/preprocessor_methods/crop.py
+++ personalCV/preprocessor_methods/crop.py
@@ -3,6 +3,29 @@ import cv2
 
 class Crop:
     
-    def __init__(self,img):
-        pass
+    def __init__(self,width,height,horizontal_flips = True,inter = cv2.INTER_AREA):
+    	
+    	self.width = width
+    	self.horizontal_flips = horizontal_flips
+    	self.height = height
+    	self.inter = inter
+
+    def preprocess(self,img):
+    	
+    	crops = []
+    	(h, w) = img.shape[:2]
+		co-ordinates = [[0, 0, self.width, self.height],[w - self.width, 0, w, self.height],[w - self.width, h - self.height, w, h],[0, h - self.height, self.width, h]]
+		delta_w = int(0.5 * (w - self.width))
+		delta_h = int(0.5 * (h - self.height))
+		co-ordinates.append([delta_w, delta_h, w - delta_w, h - delta_h])
+
+		if self.horizontal_flips:
+			m = [cv2.flip(c, 1) for c in crops]
+			crops.extend(m)
+		return np.array(crops)	
+		
+
+
+
+        
         
